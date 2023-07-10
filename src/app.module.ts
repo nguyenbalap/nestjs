7c49f0ac2e8eacdd917d9271519f54ad8cf93b3e
lapnb <lapnb.aime@gmail.com>
2023-07-10 18:01:59 +0700

    add dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { databases } from './config';
 import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PostsController } from './posts/posts.controller';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(databases),
+    DatabaseModule,
     JwtModule.register({
       global: true,
       secret: process.env.AUTH_SECRET,
